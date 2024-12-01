754105c615afd62ba133bf375cca3e620bc42d1b
Rick Spiegl <rick.spiegl@cloudflight.io>
2024-12-01 13:03:51 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,23 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = input.map { it.split("   ").let { it[0] to it[1] } }.unzip()
+        val distance = left.sorted().zip(right.sorted()).sumOf { (l, r) -> abs(l.toInt() - r.toInt()) }
+        return distance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (left, right) = input.map { it.split("   ").let { it[0] to it[1] } }.unzip()
+        val rightCounts = right.groupingBy { it }.eachCount()
+        val similarityScore = left.sumOf { it.toInt() * (rightCounts[it] ?: 0) }
+        return similarityScore
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
