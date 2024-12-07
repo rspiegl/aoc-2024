4271d3e25464089e09d72deac4f0739afc2934a6
Rick Spiegl <rick.spiegl@cloudflight.io>
2024-12-07 09:48:15 +0100

    Day 7

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.math.pow
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +20,11 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T> List<T>.permutationsWithReplacement(length: Int): List<List<T>> {
+    val permutations = size.toDouble().pow(length).toLong()
+    val binaryLists = (0 until permutations).map { it.toString(size).padStart(length, '0') }
+    return binaryLists.map { it.map { this[it.toString().toInt()] } }
+}
+
+fun Int.factorial() = (1..this).fold(1, Int::times)
